26d3551bfa6a5e1ef10c4c4fd8e4a5fcaa697b31
rdlassoe <robinsonlasso2021@itp.edu.co>
2025-06-18 00:51:25 -0500

    Creacion login funcional/validacion de credenciales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { ProductoComponent } from './page/producto/producto.component';
 import { DashboardComponent } from './page/dashboard/dashboard.component';
 import { LoginComponent } from './page/login/login.component';
 
+
 export const routes: Routes = [
     {
         path: "dashboard",
@@ -15,5 +16,6 @@ export const routes: Routes = [
     {
         path:"login",
         component: LoginComponent
-    }
+    },
+    { path: '**', redirectTo: 'login' }
 ];
--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
+  standalone: true,
   selector: 'app-login',
-  imports: [RouterModule],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  username = '';
+  password = '';
 
+  constructor(private authService: AuthService, private router: Router) { }
+
+  onSubmit() {
+    this.authService.login(this.username, this.password).subscribe({
+      next: (res) => {
+        localStorage.setItem('token', res.access_token);
+        this.router.navigate(['/dashboard']);
+      },
+      error: () => {
+        alert('Credenciales inválidas');
+      }
+    });
+  }
 }
